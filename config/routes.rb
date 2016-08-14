8e8cf799b44e28d5f65b9b1d4e02d8253025b6c8
Christine Liao <astridcliao@gmail.com>
2016-08-14 14:38:15 -0700

    added staticpages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "staticpages#root"
+
   resources :foods
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
